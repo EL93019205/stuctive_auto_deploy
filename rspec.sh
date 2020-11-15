bf078a45d1d73daf165693dce1e5fff186553eca
EL93019205 <59168351+EL93019205@users.noreply.github.com>
2020-11-15 18:49:29 +0900

    bundleコマンドを利用可能にする

--- rspec.sh
+++ rspec.sh
@@ -1,3 +1,5 @@
+# bundleコマンドを使えるようにする
+export PATH="${PATH}:/Users/el93019205/.rbenv/shims"
 # ディレクトリ移動
 cd stuctive
 # 自動テスト
--- rubocop.sh
+++ rubocop.sh
@@ -1,5 +1,6 @@
+# bundleコマンドを使えるようにする
+export PATH="${PATH}:/Users/el93019205/.rbenv/shims"
 # ディレクトリ移動
 cd stuctive
-
 # 自動静的解析
 bundle exec rubocop
